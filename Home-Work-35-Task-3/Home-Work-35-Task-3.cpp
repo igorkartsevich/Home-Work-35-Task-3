5c953a8004365bf44a2917e11f02ebdfd8301b11
igor_kartsevich <71553499+igorkartsevich@users.noreply.github.com>
2022-03-23 22:47:03 +0300

    Добавьте файлы проекта.

---
+++ Home-Work-35-Task-3/Home-Work-35-Task-3.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <filesystem>
+
+#define DEF_FILE_EXTENSION ".jpg"
+#define DEF_FILE_PATH "C:/Users/Igor Kartsevich/Pictures"
+
+int main()
+{
+    std::vector<std::filesystem::path> fileList;
+    std::string fileExtension = DEF_FILE_EXTENSION;
+    std::filesystem::path filePath(DEF_FILE_PATH);
+
+    auto recursiveGetFileNamesByExtension = [&fileList] (std::filesystem::path filePath, const std::string& fileExtension) {
+        for (auto& p : std::filesystem::recursive_directory_iterator(filePath))
+            if(p.is_regular_file())
+                if (!p.path().extension().compare(DEF_FILE_EXTENSION))
+                    fileList.emplace_back(p.path().filename());
+
+        return;
+    };
+
+    recursiveGetFileNamesByExtension(DEF_FILE_PATH, DEF_FILE_EXTENSION);
+
+    return 0;
+}
